64e14bf8a5398fa352400b616221c947511c392e
Dean-PC\Dean <dmurphy1994@hotmail.co.uk>
2022-12-04 16:06:57 +0000

    updated semantics of arrayIndexHelper

--- src/main/java/Helper/ArrayIndexHelper.java
+++ src/main/java/Helper/ArrayIndexHelper.java
@@ -2,17 +2,17 @@ package Helper;
 
 public class ArrayIndexHelper {
 
-    public static int flatten(int x, int y, int z, int xMax, int yMax, int zMax)
+    public static int flatten(int x, int y, int z, int xMax, int zMax)
     {
 //        return x + (y * xMax) + (z * xMax * yMax);
         return x + (z * xMax) + (y * xMax * zMax);
     }
 
-    public static int[] unflatten(int index, int xMax, int yMax)
+    public static int[] unflatten(int index, int xMax, int zMax)
     {
         int x = index % xMax;
-        int y = (index / xMax) % yMax;
-        int z = index / (xMax * yMax);
+        int y = index / (xMax * zMax);
+        int z = (index / xMax) % zMax;
 
         return new int[]{x, y, z};
     }
